3f4c8d7b9bfe1f3a86c8f850b4e614bab714d64d
lihuelmujica <lihuelmujica@gmail.com>
2022-07-31 15:35:24 -0300

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { SocialMediaComponent } from './components/social-media/social-media.component';
+import { SocialMediaListComponent } from './components/social-media-list/social-media-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    NavComponent,
+    SocialMediaComponent,
+    SocialMediaListComponent
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule
